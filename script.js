18bd9b193557295141fba580d8474bcfc54386f4
fmcistired <fahadchy02@gmail.com>
2023-06-05 11:40:55 +0100

    new update

--- script.js
+++ script.js
@@ -1,57 +1,36 @@
 function convertText() {
-  // Step 1: Get the input text from the HTML textarea
-  var inputText = document.getElementById("input-text").value;
-
-  // Step 2: Process the input text and convert it
-  var convertedText = processText(inputText);
-
-  // Step 4: Display the converted text
+  const inputText = document.getElementById("input-text").value;
+  const convertedText = processText(inputText);
   displayConvertedText(convertedText);
 }
 
 function processText(text) {
-  // Step 2: Split text into individual words
-  var words = text.split(/\s+/);
-  var convertedWords = [];
-
-  for (var i = 0; i < words.length; i++) {
-    var word = words[i];
-
-    // Step 5: Customize this function to retrieve the desired number of bold letters
-    var boldLetters = getBoldLetters(word);
-
+  const words = text.split(/\s+/);
+  const convertedWords = words.map(word => {
+    const boldLetters = getBoldLetters(word);
     if (boldLetters > 0) {
-      // Step 3: Format the first few letters as bold
-      var formattedWord = "<b>" + word.substr(0, boldLetters) + "</b>" + word.substr(boldLetters);
-      convertedWords.push(formattedWord);
+      return `<b>${word.substr(0, boldLetters)}</b>${word.substr(boldLetters)}`;
     } else {
-      convertedWords.push(word);
+      return word;
     }
-  }
-
-  // Step 4: Join the modified words back into a string
+  });
   return convertedWords.join(" ");
 }
 
 function getBoldLetters(word) {
-  // Step 5: Customize this function to retrieve the desired number of bold letters based on user selection
-  // Example: return 3; to bold the first 3 letters of each word
-  var length = word.length;
-  
-  if (length === 1) {
+  const length = word.length;
+  if (length === 1 || length === 2) {
     return 1;
-  }
-    if (length === 2) {
-    return 1; // Default to bolding the first 1 letter
-  } if (length === 3) {
+  } else if (length === 3) {
     return 2;
-  } if (length >= 4) {
+  } else if (length >= 4) {
     return 3;
   }
 }
 
 function displayConvertedText(text) {
-  // Step 4: Display the converted text in the HTML output element
-  var outputDiv = document.getElementById("output-text");
+  const outputDiv = document.getElementById("output-text");
   outputDiv.innerHTML = text;
 }
+
+const wordsTextarea = document.querySelector(".converter-box .wo")
